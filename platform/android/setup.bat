b5dfa77ff21d451abbdf5217c92c1325537b2cfe
Vlad Shcherban <vlad@coronalabs.com>
2019-06-12 22:26:45 -0700

    More windows improvements

--- platform/android/setup.bat
+++ platform/android/setup.bat
@@ -1,6 +1,9 @@
 @echo off
-setlocal enableextensions
+setlocal enableextensions enabledelayedexpansion
+if not exist "%APPDATA%\Corona Labs\Corona Simulator\Android Build\sdk\licenses" (
 md "%APPDATA%\Corona Labs\Corona Simulator\Android Build\sdk\licenses"
-copy /Y ..\sdk\licenses\android-sdk-license "%APPDATA%\Corona Labs\Corona Simulator\Android Build\sdk\licenses\android-sdk-license"
-echo sdk.dir=%APPDATA%\Corona Labs\Corona Simulator\Android Build\sdk > local.properties
+)
+copy /Y ..\sdk\licenses\android-sdk-license "%APPDATA%\Corona Labs\Corona Simulator\Android Build\sdk\licenses\android-sdk-license" >nul
+set PT=sdk.dir=%APPDATA%\Corona Labs\Corona Simulator\Android Build\sdk
+echo|set /p="%PT:\=/%" > local.properties
 endlocal
