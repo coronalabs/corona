15655bfe0c303e9a19e4e20b9c2b43aa9a667e92
Vlad Shcherban <vlad@coronalabs.com>
2019-11-13 09:40:28 -0500

    Updating to Xcode 11.2.1 and Gradle plugin to 3.5.2

--- platform/android/build.gradle.kts
+++ platform/android/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath("com.android.tools.build:gradle:3.5.2")
         classpath("de.undercouch:gradle-download-task:3.4.3")
         classpath("com.beust:klaxon:5.0.1")
     }
