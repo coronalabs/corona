0861b432e67d94f2f33d9caeb6ff735b6d1abadb
Vlad Shcherban <vlad@coronalabs.com>
2020-03-28 16:47:04 -0400

    Fixing build at least for debug

--- platform/windows/Build.Tools/CoronaLabsInc.Sign.bat
+++ platform/windows/Build.Tools/CoronaLabsInc.Sign.bat
@@ -3,7 +3,8 @@ REM ----------------------------------------------------------------------------
 REM Batch file used to digitally sign a given file with Corona Labs' certificate.
 REM ---------------------------------------------------------------------------------------
 
-if NOT EXIST "%~dp0CoronaLabsInc.pfx" goto :eof
+if NOT EXIST "%~dp0CoronaLabsInc.pfx" goto :OnSkipSigning
+if "%WIN_CERT_PASSWORD%"=="" goto :OnSkipSigning
 
 REM Validate arguments. If incorrect, then echo out how to use this batch file.
 if "%~1"=="" goto OnShowCommandLineHelp
@@ -17,19 +18,19 @@ REM Digitally sign the given file.
 echo Digitally signing file: %~1
 call "%VS120COMNTOOLS%vsvars32.bat"
 
-signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "zxcvbnm,./" /t http://timestamp.digicert.com %programNameArgument% "%~1"
+signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "%WIN_CERT_PASSWORD%" /t http://timestamp.digicert.com %programNameArgument% "%~1"
 if errorlevel 0 goto:eof
 echo "Signin failed. Using fallback timestamp server 1!"
-signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "zxcvbnm,./" /t http://timestamp.verisign.com/scripts/timestamp.dll %programNameArgument% "%~1"
+signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "%WIN_CERT_PASSWORD%" /t http://timestamp.verisign.com/scripts/timestamp.dll %programNameArgument% "%~1"
 if errorlevel 0 goto:eof
 echo "Signin failed. Using fallback timestamp server 2!"
-signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "zxcvbnm,./" /t http://timestamp.globalsign.com/scripts/timestamp.dll %programNameArgument% "%~1"
+signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "%WIN_CERT_PASSWORD%" /t http://timestamp.globalsign.com/scripts/timestamp.dll %programNameArgument% "%~1"
 if errorlevel 0 goto:eof
 echo "Signin failed. Using fallback timestamp server 3!"
-signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "zxcvbnm,./" /t http://tsa.starfieldtech.com %programNameArgument% "%~1"
+signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "%WIN_CERT_PASSWORD%" /t http://tsa.starfieldtech.com %programNameArgument% "%~1"
 if errorlevel 0 goto:eof
 echo "Signin failed. Using fallback timestamp server 4!"
-signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "zxcvbnm,./" /t http://timestamp.geotrust.com/tsa %programNameArgument% "%~1"
+signtool sign /f "%~dp0CoronaLabsInc.pfx" /p "%WIN_CERT_PASSWORD%" /t http://timestamp.geotrust.com/tsa %programNameArgument% "%~1"
 if errorlevel 0 goto:eof
 echo "NO TIMESTAMP FALLBACK WORKED!"
 exit /b 1
@@ -43,3 +44,6 @@ echo Usage:    CoronaLabsInc.Sign.bat [FilePath] [ProgramName]
 echo   [FilePath]         Path to the file that you want to digitally sign.
 echo   [ProgramName]      Optional: Program name to be displayed in the UAC prompt.
 exit /b 1
+
+:OnSkipSigning
+echo Skipping singing becaues password or key is not found
