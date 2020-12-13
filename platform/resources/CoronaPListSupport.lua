01356dbe39cb33146c411d118dac7a3151286678
Vlad Svoka <vlad@coronalabs.com>
2020-12-13 16:24:55 +0200

    Apple: reverting CFBundleVersion to old format (#208)
    users reported that they were relying on the old format

--- platform/resources/CoronaPListSupport.lua
+++ platform/resources/CoronaPListSupport.lua
@@ -216,6 +216,10 @@ function CoronaPListSupport.modifyPlist( options )
 			-- set it to the current date and time which is unique for the app and human readable
 			bundleVersionSource = (infoPlist.CFBundleVersion == "@BUNDLE_VERSION@") and "set by Simulator" or "set by Info.plist"
 			infoPlist.CFBundleVersion = os.date("%Y.7%m.7%d%H%M")
+			local datedata = os.date( "!*t")
+			if datedata.year > 2020 then
+				infoPlist.CFBundleVersion = datedata.year .. '.' .. datedata.month.."." .. datedata.day .. os.date("!%H%M")
+			end
 		end
 
 		local version = options.bundleversion or "1.0.0"
