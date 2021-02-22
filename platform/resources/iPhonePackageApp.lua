65efd8a3469d42f3613c3e34d32304a7cccc7df3
Scott H Tech <scottrules44@gmail.com>
2021-02-22 12:44:09 -0500

    iOS: fixing on On Demand Resources packaging (#230)

--- platform/resources/iPhonePackageApp.lua
+++ platform/resources/iPhonePackageApp.lua
@@ -1018,7 +1018,12 @@ local function packageApp( options )
 			setStatus("Creating IPA for store submission...")
 			-- note we move the app to the "Payload" directory to preserve permissions and for speed which means the .app doesn't exist anymore
 			runScript( "mv " .. quoteString(makepath(options.dstDir, options.dstFile..".app")) .." ".. quoteString(makepath(ipaTmpDir, "Payload")) )
-
+			
+			--move odr resources to "Payload" folder
+			if odrOutputDir then
+				runScript( "mv " .. quoteString(makepath(odrOutputDir)) .." ".. quoteString(makepath(ipaTmpDir, "Payload")) )
+			end
+			
 			if bundleSwiftSupportDir then
 				runScript( "mv " .. bundleSwiftSupportDir .." ".. quoteString(ipaTmpDir) )
 			end
