d68b98e3905efd0c7eeb64ef52f4adad34b1ea10
vitaly <bakeinflash@gmail.com>
2021-08-21 20:03:28 +0300

    Revert "NxS: attaching utf8 plugin"
    This reverts commit 44abe50db04cf354a913bfde7c2a552958510f3d.

--- librtt/Rtt_LuaContext.cpp
+++ librtt/Rtt_LuaContext.cpp
@@ -94,10 +94,6 @@ extern "C" {
 
 #ifdef Rtt_NXS_ENV
 	int luaopen_network(lua_State* L);
-
-	extern "C" {
-		int luaopen_plugin_utf8(lua_State* L);
-	}
 #endif
 
 namespace Rtt
@@ -813,9 +809,7 @@ LuaContext::InitializeLuaCore( lua_State* L )
 		{ "nnTextField", Lua::Open< luaload_nnTextField > },
 		{ "nnTextBox", Lua::Open< luaload_nnTextBox > },
 		{ "nnNativeAlert", Lua::Open< luaload_nnNativeAlert > },
-
-		{ "plugin.utf8", luaopen_plugin_utf8 },
-
+	
 #endif
 
 		{NULL, NULL}
