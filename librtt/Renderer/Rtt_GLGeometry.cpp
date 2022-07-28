4e56bc8f7b1b98d4e65a3179d369c2538dd155ce
Jeremy <53489032+clang-clang-clang@users.noreply.github.com>
2022-07-28 15:34:38 +0800

    add copyright notice

--- librtt/Renderer/Rtt_GLGeometry.cpp
+++ librtt/Renderer/Rtt_GLGeometry.cpp
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////
 //
-// This file is part of the Corona game engine.
+// This file is part of the Solar2D game engine.
+// With contributions from DIANCHU TECHNOLOGY
 // For overview and more information on licensing please refer to README.md 
 // Home page: https://github.com/coronalabs/corona
 // Contact: support@coronalabs.com
