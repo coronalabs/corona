b511cab642329ab760afc26f36ed02741d82ea27
Vlad Svoka <vlad@coronalabs.com>
2022-08-01 09:01:42 -0400

    macOS: fixing VS Code instances lingering (#432)

--- librtt/Renderer/Rtt_GLGeometry.cpp
+++ librtt/Renderer/Rtt_GLGeometry.cpp
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 // This file is part of the Solar2D game engine.
-// With contributions from DIANCHU TECHNOLOGY
+// With contributions from Dianchu Technology
 // For overview and more information on licensing please refer to README.md 
 // Home page: https://github.com/coronalabs/corona
 // Contact: support@coronalabs.com
