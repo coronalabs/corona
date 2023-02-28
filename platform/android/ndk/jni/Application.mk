ffac32c2123b8e20642ac24b334fb173a6de56a0
Vlad Svoka <shchvova@gmail.com>
2023-02-27 22:10:04 -0800

    deleting antiquated build files

--- platform/android/ndk/jni/Application.mk
+++
@@ -1,9 +0,0 @@
-#APP_MODULES      := corona 
-#APP_PROJECT_PATH := $(call my-dir)/project
-APP_PLATFORM := android-14
-#APP_ABI := armeabi 
-#APP_ABI := armeabi armeabi-v7a
-APP_ABI := armeabi-v7a
-
-# Box2D needs this
-APP_STL := stlport_static
