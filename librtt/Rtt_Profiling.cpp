5b520b649e641012d391ca1ef1587f8b901d6b47
ggcrunchy <steve@xibalbastudios.com>
2023-04-06 19:33:36 -0500

    Stub profiling files and added them to Mac project

---
+++ librtt/Rtt_Profiling.cpp
@@ -0,0 +1,8 @@
+//
+//  Rtt_Profiling.cpp
+//  ratatouille
+//
+//  Created by Steven Johnson on 4/6/23.
+//
+
+#include <stdio.h>
---
+++ librtt/Rtt_Profiling.h
@@ -0,0 +1,12 @@
+//
+//  Rtt_Profiling.h
+//  ratatouille
+//
+//  Created by Steven Johnson on 4/6/23.
+//
+
+#ifndef Rtt_Profiling_h
+#define Rtt_Profiling_h
+
+
+#endif /* Rtt_Profiling_h */
