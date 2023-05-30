685ff83bb1eac0576d75402fbebc03acc0fe2271
Scott Harrison <scottrules44@gmail.com>
2023-05-29 23:08:46 -0400

    Core: Fixes to Display.Capture on start up (#566)
    Co-authored-by: Vlad Svoka <vlad@coronalabs.com>

--- platform/mac/GLView.mm
+++ platform/mac/GLView.mm
@@ -358,16 +358,16 @@ NSOpenGLPixelFormatAttribute attributes1 [] = {
 
 	[[self openGLContext] makeCurrentContext];
 
+    
+    
 	// This should be called by the layer, not NSTimer!!!
 	// That's b/c the OGL context is valid and ready for new OGL commands
 	if ( isReady && fRuntime != NULL && fRuntime->IsProperty(Rtt::Runtime::kIsApplicationLoaded))
 	{
 		fRuntime->Render();
 	}
-    if([self openGLContext]){
-        [[self openGLContext] flushBuffer];
-    }
     
+    [[self openGLContext] flushBuffer];
 }
 
 - (void)setDelegate:(id< GLViewDelegate >)delegate
