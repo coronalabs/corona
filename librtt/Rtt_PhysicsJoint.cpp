0c869d369e854f5b69a21519c23213fd96ed57f3
Huynh Quoc Khanh aka Kan <70838508+Kan-Kzeit@users.noreply.github.com>
2024-05-05 21:59:56 +0700

    Revert "Fix getReactionForce not returning value according to inverse delta time"
    This reverts commit 92cd04148c0df498ccf9b00cdf5f392b768d9cdd.

--- librtt/Rtt_PhysicsJoint.cpp
+++ librtt/Rtt_PhysicsJoint.cpp
@@ -352,13 +352,9 @@ PhysicsJoint::getReactionForce( lua_State *L )
 	if ( baseJoint )
 	{
 		Runtime& runtime = * LuaContext::GetRuntime( L );
-		float32 deltaTime = (float)runtime.GetFPS();
-		float32 inverseDeltaTime = 1 / deltaTime;
-
-		b2Vec2 reactionForce = baseJoint->GetReactionForce(inverseDeltaTime);
-
-		Rtt_Real px = Rtt_FloatToReal(reactionForce.x);
-		Rtt_Real py = Rtt_FloatToReal(reactionForce.y);
+		float32 inverseDeltaTime = (float)runtime.GetFPS();
+		Rtt_Real px = Rtt_FloatToReal( baseJoint->GetReactionForce( inverseDeltaTime ).x );
+		Rtt_Real py = Rtt_FloatToReal( baseJoint->GetReactionForce( inverseDeltaTime ).y );
 		
 		lua_pushnumber( L, px );
 		lua_pushnumber( L, py );
