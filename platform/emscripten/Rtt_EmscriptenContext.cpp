8b253a82a913c14903f80ce910318bb20f25bdc7
Kan <70838508+kan6868@users.noreply.github.com>
2025-07-08 19:10:11 +0700

    Hack: resize to fit to screen

--- platform/emscripten/Rtt_EmscriptenContext.cpp
+++ platform/emscripten/Rtt_EmscriptenContext.cpp
@@ -570,12 +570,7 @@ namespace Rtt
 
 		// hack
 #ifdef EMSCRIPTEN
-		if ((stricmp(fRuntimeDelegate->fScaleMode.c_str(), "zoomStretch") == 0) || (stricmp(fRuntimeDelegate->fScaleMode.c_str(), "zoomEven") == 0))
-		{
-			EM_ASM_INT({	window.dispatchEvent(new Event('resize')); });
-		}
-
-		emscripten_set_element_css_size("canvas", (int)(scaledWidth / devicePixelRatio), (int)(scaledHeight / devicePixelRatio));
+		EM_ASM_INT({	window.dispatchEvent(new Event('resize')); });
 #endif
 
 		return true;
