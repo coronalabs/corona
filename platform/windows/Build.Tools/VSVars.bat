fb4b583b7297ca8e9403f65821fe3777773cf82d
Vlad Svoka <130387+Shchvova@users.noreply.github.com>
2025-08-24 21:37:11 +0000

    Fixing windows build (#831)

--- platform/windows/Build.Tools/VSVars.bat
+++ platform/windows/Build.Tools/VSVars.bat
@@ -1,16 +1,27 @@
 @echo off
 
-echo Setup command line
-for %%V in ("%VS120COMNTOOLS%vsvars32.bat"
-           ,"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
-           ,"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" 
-           ) do (
-    where signtool >nul 2>nul
-    if %errorlevel% neq 0 if exist %%V (
-        echo Using %%V
+echo Setting up Visual Studio C++ CLI
+for %%V in (
+    "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+    "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars32.bat"
+    "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+    "%VS120COMNTOOLS%vsvars32.bat"
+) do (
+    echo trying %%V
+    if exist %%V (
+        echo It exists
         call %%V
+        where signtool >nul 2>nul
+        if %errorlevel% == 0 (
+            echo Using %%V
+            goto :found
+        )
     )
-)   
+)
+
+:found
 
 where signtool >nul 2>nul
 if %errorlevel% neq 0 (
