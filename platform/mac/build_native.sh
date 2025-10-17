55ea1a360f363d9439e775df47d8827baf9ba2a8
Scott Harrison <scottrules44@gmail.com>
2025-10-17 16:51:50 -0400

    Fixes to compile intel for CoronaCards on Mac

--- platform/mac/build_native.sh
+++ platform/mac/build_native.sh
@@ -9,4 +9,12 @@ fi
 
 echo "Building CoronaCards For Mac"
 
-xcodebuild SYMROOT="$path/build" -project "${path}"/ratatouille.xcodeproj -target CoronaCards -configuration Release 2>&1 | tee -a "$FULL_LOG_FILE" | egrep -v "$XCODE_LOG_FILTERS"
+xcodebuild \
+  SYMROOT="$path/build" \
+  -project "${path}/ratatouille.xcodeproj" \
+  -target CoronaCards \
+  -configuration Release \
+  ARCHS="arm64 x86_64" \
+  ONLY_ACTIVE_ARCH=NO \
+  2>&1 | tee -a "$FULL_LOG_FILE" | egrep -v "$XCODE_LOG_FILTERS"
+
