9709c9d16190c34d38218ffc02de5792ef6592c1
ggcrunchy <steve@xibalbastudios.com>
2026-09-16 22:53:18 -0600

    Hmm, missed definition of VulkanRenderer's BeginFrame()

--- librtt/Renderer/Rtt_VulkanRenderer.cpp
+++ librtt/Renderer/Rtt_VulkanRenderer.cpp
@@ -596,7 +596,7 @@ VulkanRenderer::~VulkanRenderer()
 }
 
 void
-VulkanRenderer::BeginFrame( Real totalTime, Real deltaTime, const TimeTransform * defTimeTransform, Real contentScaleX, Real contentScaleY, bool isCapture )
+VulkanRenderer::BeginFrame( Real totalTime, Real deltaTime, Real contentScaleX, Real contentScaleY, bool isCapture )
 {
 	InitializePipelineState();
 
